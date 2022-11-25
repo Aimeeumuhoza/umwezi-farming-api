1afd3b4b8808a6b664415a2bb91a6b4d6466db5c
yschristian <yubahwesc@gmail.com>
2022-11-25 23:51:23 +0200

    change something through

--- src/middleware/verifyToken.js
+++ src/middleware/verifyToken.js
@@ -26,21 +26,22 @@ const verifyTokenAndAdmin = async(req,res,next) => {
 const verifyTokenAndfamer = (req,res,next)=>{
   const token =  req.headers.authorization
   const user = verify(token)
-    if(user.data._id === req.params._id || "client" ){
+    if(user.data._id === req.params._id || "farmer" ){
       next()
     }else{
       res.status(500).json("you don't have access")
     }
 }
 
-const verifyTokenAndpersonsale = (req,res,next)=>{
+const verifyTokenAndAuhorization = (req,res,next)=>{
   const token =  req.headers.authorization
   const user = verify(token)
-    if(user.data._id === req.params._id || "client" ){
+  console.log(user);
+    if(user.data._id === req.params._id ){
       next()
     }else{
       res.status(500).json("you don't have access")
     }
 }
 
-module.exports = {verifyToken,verifyTokenAndAdmin,verifyTokenAndfamer,verifyTokenAndpersonsale }
+module.exports = {verifyToken,verifyTokenAndAdmin,verifyTokenAndfamer,verifyTokenAndAuhorization }
