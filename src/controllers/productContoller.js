baf08661eb0f4fcfcf670778f485e657fce5138a
yschristian <yubahwesc@gmail.com>
2022-11-26 16:56:54 +0200

    farmercan create there product

--- src/controllers/productContoller.js
+++ src/controllers/productContoller.js
@@ -4,14 +4,17 @@ const createproduct = async (req, res) => {
 
   try {
     const result = await cloudinary.uploader.upload(req.file.path)
-
+    // const id= req.user.data.id
+    // console.log(id);
     const product = await Product.create({
+      UserId: req.body.UserId,
       Title: req.body.Title,
       Description: req.body.Description,
       Categories: req.body.Categories,
       Image: result.secure_url,
-      Price: req.body.Price,
-      Address: req.body.Address
+      price: req.body.price,
+      Address: req.body.Address,
+      CountInStock: req.body.CountInStock
     })
 
     return res.status(200).json({ message: "product created successfully", product })
@@ -40,12 +43,20 @@ const getAllproduct = async (req, res) => {
     console.log(err);
   }
 }
+const getUserProduct = async (req, res) => {
+  try {
+    const Products = await Product.find({userId: req.params.UserId})
+    return res.status(200).json({message:"user products are retrieved",Products})
+  } catch (error) {
+    return res.status(400).json({error:error.message})
+  }
+}
 const updateproduct = async (req, res) => {
 
   try {
     const id = req.params._id
     const product = await Product.findByIdAndUpdate(id, req.body)
-    res.status(200).json({ messsage: "product updated", product })
+    return res.status(200).json({ messsage: "product updated", product })
   }
   catch (error) {
     console.log(error)
@@ -62,4 +73,4 @@ const deleteproduct = async (req, res) => {
   }
 }
 
-module.exports = { createproduct, getProduct, getAllproduct, updateproduct, deleteproduct }
+module.exports = { createproduct, getProduct, getAllproduct, updateproduct, deleteproduct, getUserProduct }
