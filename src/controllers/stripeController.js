009112c79df0d05d791ee3330e24b44b8750ac97
yschristian <yubahwesc@gmail.com>
2022-12-07 14:58:19 +0200

    payment api

---
+++ src/controllers/stripeController.js
@@ -0,0 +1,17 @@
+const stripe = require("stripe")(process.env.STRIPE_KEY)
+
+const  payment = async(req,res)=> {
+    stripe.charges.create({
+       source: req.body.tokenId,
+       amount: req.body.amount,
+       currency: "usd",
+    },(stripeErr, stripeRes)=>{
+       if(stripeErr){
+           res.status(500).json({error:error.message})
+       }else{
+           res.status(200).json({message:"payment successfully",stripeRes})
+       }
+    });
+}
+
+module.exports = payment
