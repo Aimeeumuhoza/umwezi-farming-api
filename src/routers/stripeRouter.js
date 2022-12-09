a79c5b6917c1c7bd8530cf236f851ce99f0b9f27
yschristian <yubahwesc@gmail.com>
2022-12-09 17:14:42 +0200

    payment

--- src/routers/stripeRouter.js
+++ src/routers/stripeRouter.js
@@ -1,7 +1,8 @@
 const express = require("express");
+const payment = require("../controllers/stripeController")
 
 const stripeRouter = express()
 
-stripeRouter.post("/payment",paymentController.payment)
+stripeRouter.post("/payment",payment)
 
 module.exports = stripeRouter
