933fb5d5b20b68c4c5689b26e12113dba0110be8
yschristian <yubahwesc@gmail.com>
2022-12-11 23:28:45 +0200

    stripe

--- src/controllers/stripeController.js
+++ src/controllers/stripeController.js
@@ -1,49 +1,44 @@
 const Stripe = require("stripe");
+
+const dotenv = require("dotenv")
+dotenv.config()
 const stripe = Stripe(process.env.STRIPE_KEY);
 
 const pay = async (req, res) => {
     try {
-        const { name } = req.body;
-        if (!name) return res.status(400).json({ message: "Please enter a name" });
+        const {name,amount} = req.body;
+        if(!name) return res.status(400).json({message:"Please enter the name"});
+
         const paymentIntent = await stripe.paymentIntents.create({
-            amount: Math.round(25 * 100),
-            currency: "usd",
-            payment_method_types: ["card"],
-            metadata: { name },
+            amount:amount * 100,
+            metadata:{name},
+            currency:"usd",
+            payment_method_types:['card']
         });
         const clientSecret = paymentIntent.client_secret;
-        res.json({ message: "Payment initiated", clientSecret });
+        res.status(200).json({message:"payment iNitiated",clientSecret});
     } catch (err) {
-        console.error(err);
-        res.status(500).json({ message: "Internal server error" });
+        console.log(err);
+        res.status(200).json({error:error.message})
     }
 }
 
 const stripecard = async(req, res)=>{
 
-    const sig = req.headers["stripe-signature"];
-    let event;
-    try {
-      event = await stripe.webhooks.constructEvent(
-        req.body,
-        sig,
-        process.env.STRIPE_WEBHOOK_SECRET
-      );
-    } catch (err) {
-      console.error(err);
-      res.status(400).json({ message: err.message });
+    stripe.ChargesResource.create({
+        source:req.body.tokenId,
+        amount:req.body.amount,
+        currency:'usd'
+    },
+    (stripeError,stripeRes) =>{
+        if(stripeError){
+            res.status(500).json(stripeError);
+        }
+        else{
+            res.status(200).json(stripeRes);
+        }
     }
-  
-    // Event when a payment is initiated
-    if (event.type === "payment_intent.created") {
-      console.log(`${event.data.object.metadata.name} initated payment!`);
-    }
-    // Event when a payment is succeeded
-    if (event.type === "payment_intent.succeeded") {
-      console.log(`${event.data.object.metadata.name} succeeded payment!`);
-      // fulfilment
-    }
-    res.json({ ok: true });
+    );
 }
 
 module.exports = {pay,stripecard}
